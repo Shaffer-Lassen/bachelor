2307a8bb722b7c3d2541d4729072b459deb9d7b3
Shaffer-Lassen <93978025+Shaffer-Lassen@users.noreply.github.com>
2025-01-12 16:52:29 +0100

    Update training.sh

--- scripts/training.sh
+++ scripts/training.sh
@@ -1,29 +1,8 @@
-# Activate the virtual enviroment
-
-source /home/mkv334/mde_env/bin/activate
-
-#Go to executing dir
-cd /home/mkv334/csl/MDE02/pythonProject
-
+cd ..
 CUDA_DEVICE=0
 LOSS=l1
 EPOCHS=30
 DATA=NYUv2
-M1=
-M2=swin
-M3=scratch
-M4=naive
+MODEL=scratch
 
-
-#Run python training commands
-#echo "Starting training Dino"
-#python train.py --epochs $EPOCHS  --model $M1 --loss $LOSS --cuda $CUDA_DEVICE --dataset $DATA
-
-#echo "Starting training ScratchModel"
-#python train.py  --epochs $EPOCHS --model $M2 --loss $LOSS --cuda $CUDA_DEVICE  --dataset $DATA
-
-#echo "Starting training DenseModel"
-#python train.py --epochs $EPOCHS --model $M3 --loss $LOSS --cuda $CUDA_DEVICE --dataset $DATA --pretrained False
-
-#echo "Starting training ResModel"
-python train.py --epochs $EPOCHS --model $M4 --loss $LOSS --cuda $CUDA_DEVICE --dataset $DATA --pretrained False
+python3 train.py --epochs $EPOCHS --model $MODEL --loss $LOSS --cuda $CUDA_DEVICE --dataset $DATA --pretrained False
