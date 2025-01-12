e388137708e6e7b0063330c1bc99303e4e023917
Shaffer-Lassen <93978025+Shaffer-Lassen@users.noreply.github.com>
2025-01-12 16:51:31 +0100

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,17 +1,10 @@
-#!/bin/bash
+cd ..
 
-# Activate the virtual environment
-source /home/mkv334/mde_env/bin/activate
-
-# Go to the executing directory
-cd /home/mkv334/csl/MDE02/pythonProject
-
-# Run python training commands
 echo "Predicting depth(s)"
 python run.py \
-	--image_path /home/mkv334/csl/MDE02/pythonProject/assets/example_images \
-	--save_dir /home/mkv334/csl/MDE02/pythonProject/output \
-	--checkpoint /home/mkv334/csl/MDE02/pythonProject/checkpoints/20250111_023801_scratch_epoch10.pth  \
+	--image_path  \
+	--save_dir \
+	--checkpoint  \
 	--side_by_side \
-	--model scratch \
+	--model \
 	--color_map spectral \
