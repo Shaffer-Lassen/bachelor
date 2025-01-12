ef2c786b7d4fa8ca02a6ca4b727a4549c13f60f7
Shaffer-Lassen <93978025+Shaffer-Lassen@users.noreply.github.com>
2025-01-12 16:47:57 +0100

    Update encoder_depth.sh

--- scripts/encoder_depth.sh
+++ scripts/encoder_depth.sh
@@ -3,13 +3,9 @@
 #This script trains the Resnet based model on three different encoder depth: Resnet50, ResNet101 and Resnet 152.
 #Performance can be compared by running the eval.sh script on each of the generated .pth files.
 
-#Activate enviroment
-source /home/mkv334/mde_env/bin/activate
-
-#Go to executing dir
-cd /home/mkv334/csl/MDE02/pythonProject
+cd ..
 
 #Train models with default hyperparameters but different depths
-python train.py --model res50 --epochs 10 --pretrained True --cuda 1
-python train.py --model res101 --epochs 10 --pretrained True --cuda 1
-python train.py --model res152 --epochs 10 --pretrained True --cuda 1
+python3 train.py --model res50 --epochs 10
+python3 train.py --model res101 --epochs 10
+python3 train.py --model res152 --epochs 10
